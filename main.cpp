9f72575c3f5ba77e8232fa41077f72d0b0de2230
awillats <awillats3@gatech.edu>
2019-02-21 10:31:26 -0500

    initial commit with hello world. Next, will import good code from previous modules

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hmm
+//
+//  Created by Adam Willats on 2/21/19.
+//  Copyright © 2019 Adam Willats. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
