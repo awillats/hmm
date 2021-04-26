c392ad44e56fb8b52618f81861eb7a31a7cefca3
Adam Willats <awillats3@gatech.edu>
2021-04-26 13:39:58 -0400

    extracted docs/html to docs

---
+++ docs/print_funs_8cpp.js
@@ -0,0 +1,7 @@
+var print_funs_8cpp =
+[
+    [ "blockPrint", "print_funs_8cpp.html#a4a096fa17f0aa53956c30774420928d0", null ],
+    [ "printMat", "print_funs_8cpp.html#a141d01c07c9474d12d6fd94c85970199", null ],
+    [ "printVec", "print_funs_8cpp.html#a3fd35d0e5ba4813e333f7ea7995cbb21", null ],
+    [ "printVecAsBlock", "print_funs_8cpp.html#abe447dc230ab6dc5a55c55bd188e314d", null ]
+];
---
+++ docs/print_funs_8hpp.js
@@ -0,0 +1,8 @@
+var print_funs_8hpp =
+[
+    [ "blockPrint", "print_funs_8hpp.html#ac28f245edc7a3ca8d299a661f3ce15da", null ],
+    [ "operator<<", "print_funs_8hpp.html#a752c44978f0f5abde8919968032102bb", null ],
+    [ "printMat", "print_funs_8hpp.html#aa2deae551f32cc6b830ae4875e5a2d08", null ],
+    [ "printVec", "print_funs_8hpp.html#a2e91049a793c354a83ffc67488e0186f", null ],
+    [ "printVecAsBlock", "print_funs_8hpp.html#a2e23f1e8b7302b941fae637ec860ebaf", null ]
+];
